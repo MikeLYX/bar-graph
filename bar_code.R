1330cc39368070da94b5a113d7bfa9c43b0646f5
Xiang-Yun Huang <11107100103@student.cumtb.edu.cn>
2017-08-24 09:25:09 +0800

    Update bar_code.R

--- bar_code.R
+++ bar_code.R
@@ -1,14 +1,158 @@
-# 堆积条形图
+# 图0
+library(ISLR) 
+data(NCI60) # NCI 60 Data
+library(RColorBrewer)
+dat <- as.data.frame(table(NCI60$labs))
+order_dat <- dat[order(dat$Freq,decreasing = TRUE),]
+num <- order_dat[,2]
+names(num) <- order_dat[,1]
+op <- par(mar=c(4,8,2,1))
+barplot(num, col = brewer.pal(12,"Set3"), axes = FALSE, border=NA, axisnames=FALSE,
+        horiz=TRUE,xlab="numbers",ylab="",legend.text = TRUE)		
+xlabs <- seq(0, 9, by = 1)
+ylabs <- order_dat[,1]		
+axis(1, labels = xlabs, at = xlabs, las = 1) # x-axis
+axis(2, labels = ylabs, at = seq(14), las = 1,lwd =1) # y-axis
+par(op)
+
+# 图1  泊松分布		
+set.seed(123456)
+barPois <- table(stats::rpois(1000, lambda = 5))
+barplot(barPois, col = "lightblue",border = "white",main = "The Poisson Distribution")
+
+# 图2 
+data(NCI60,package ="ISLR") # 加载数据
+dat <- as.data.frame(table(NCI60$labs))
+order_dat <- dat[order(dat$Freq,decreasing = TRUE),]
+num <- order_dat[,2]
+names(num) <- order_dat[,1]
+op <- par(mar=c(2,7,1,1))
+plot(c(0,10),c(0,17),type="n",axes = FALSE,ann=FALSE)
+xlabs <- seq(0,10,by=1) # 下面一行是关键
+ylabs <- seq(0.7,length(num)+2.3,length.out=length(num))
+ylabs_names <- names(num)
+axis(1, labels = xlabs, at = xlabs, las = 1,col = "gray") # xaxis
+axis(2, labels = ylabs_names, at = ylabs, las = 1,col="white")
+barplot(num,add=TRUE,col="lightblue",axes = FALSE,
+		axisnames=FALSE,border ="white",
+		horiz=TRUE,xlab="numbers",ylab="")
+
+# 图3
+data(NCI60,package ="ISLR") # 加载数据		
+myData <- sort(table(NCI60$labs))		
+par(mar=c(5,2.5,1,1))	
+barCenters <- barplot(myData,col = "lightblue",axes = FALSE,
+			axisnames=FALSE,border ="white")				
+text(x = barCenters, y = par("usr")[3]-.2, srt = 45,
+     adj = 1, labels = names(myData), xpd = TRUE)
+axis(2, labels = seq(0,9,by=1), at = seq(0,9,by=1),  las = 1 , col = "gray") 
+
+# 图4
+
+
+
+# 图5  点图  mtcars
+mtcars4 <- mtcars[mtcars$cyl == 4,]
+mtcars6 <- mtcars[mtcars$cyl == 6,]
+mtcars8 <- mtcars[mtcars$cyl == 8,]
+myData <- rbind( mtcars4[order(mtcars4$mpg,decreasing = FALSE ), ],
+		 mtcars6[order(mtcars6$mpg,decreasing = FALSE ), ],
+		 mtcars8[order(mtcars8$mpg,decreasing = FALSE ), ] )
+
+pdf(file = "dotplot.pdf",width=6,height=8)
+par(mar =c(2,8,0,4) )
+with(myData,{
+	barCenters <- barplot(mpg,horiz = TRUE,axes = FALSE,xlim=c(0,35),
+				col = rev(paste0("lightblue",seq(3)))[as.factor(cyl)],
+				space =4.5, border = "white",width = .1)
+	text(y = barCenters, x = par("usr")[3],
+		 adj = 1, labels = rownames(myData), xpd = TRUE) # 纵轴标签
+	points(y = barCenters, x = mpg, pch= 19, cex = 2.5,
+			col = rev(paste0("lightblue",seq(3)))[as.factor(cyl)] )  # 加点
+	text(y = barCenters, x = mpg, labels = mpg,cex=.5) # 加数字
+	legend("topright", legend = c("8","6","4"),title = "cyl",
+			col = c("lightblue1","lightblue2","lightblue3"),pch = 19,
+			border = "white",box.col = "white", pt.cex = 2.5,cex = 1.5) # 加图例
+	axis(1, labels = seq(0,35,by=5), at = seq(0,35,by=5),  las = 1 , col = "gray")	# 横轴 	
+})
+dev.off()
+
+# 图6
+data(NCI60,package ="ISLR") # 加载数据
+dat <- as.data.frame(table(NCI60$labs))
+order_dat <- dat[order(dat$Freq,decreasing = TRUE),]
+num <- order_dat[,2]
+names(num) <- order_dat[,1]
+op <- par(mar=c(2,1,1,1))
+plot(c(0,10),c(0,17),type="n",axes = FALSE,ann=FALSE)
+xlabs <- seq(0,10,by=1)
+axis(1, labels = xlabs, at = xlabs, las = 1) # x-axis
+mycolors <- yarrr::piratepal(palette = "info2") 
+barplot(num,add=TRUE,col=mycolors,axes = FALSE,
+axisnames=FALSE,border ="white",
+args.legend=list(border = "white",box.col="white"),
+horiz=TRUE,xlab="numbers",ylab="",legend.text = TRUE)
+box(col = "gray")
+par(op)
+
+# 图8 同图2
+
+# 图9  Gambia儿童疟疾
+
+library(sjPlot)
+library(sjmisc)
+library(ggplot2)
+
+data(gambia,package="geoR")
+dat1 <- subset(gambia,netuse==0) # 0 没有使用蚊帐
+dat1$Index <- rep(0,dim(dat1)[1])
+dat2 <- subset(gambia,netuse==1 & treated == 0)  # 1
+dat2$Index <- rep(1,dim(dat2)[1])
+dat3 <- subset(gambia,netuse==1 & treated == 1)  # 2 
+dat3$Index <- rep(2,dim(dat3)[1])
+mydata <- rbind(dat1,dat2,dat3)
+
+sjp.setTheme(theme.font="Times",geom.outline.color = "white",
+	     axis.linecolor = "white", base = theme_classic())
+
+pdf(file="bed_net.pdf",width = 6,height = 5.25)
+sjp.grpfrq(	mydata$Index,mydata$pos,  geom.colors = "Set1",
+			axis.titles="Childhood malaria in the Gambia",
+			axis.labels= c("net not use","net not treated","net treated"),
+			legend.title="RDT")
+dev.off()
+
+# 图10
 barplot(t(VADeaths), col = rev(paste0("lightblue",seq(4))),
 		border = "white", axes = FALSE, ylim = c(0, 241.4),
 		legend.text = colnames(VADeaths),
 		args.legend = list(x = "topleft",border = "white",
-						   box.col = "white", pt.cex = 2) )	
-		
+				   box.col = "white", pt.cex = 2) )		
 axis(2, labels = seq(0,240,by=40), at = seq(0,240,by=40),  las = 1 , col = "gray")		
 		
-	
-# 正着的条形图	
+# 图11 图12	
+
+# 图13 复合条形图
+par(mfrow = c(1,6),mar = c(4.5,2.5,0,1.5))
+layout(mat = matrix(seq(6), ncol = 6, byrow = TRUE))
+for(i in seq(6)){
+	plot(c(0,2),c(0,850), axes = FALSE, type = "n", xlab = "",ylab = "")
+	barplot(UCBAdmissions[,,i], col = c("lightblue3","lightblue1"),axes = FALSE,
+		border = "white", xlab = paste0("Dept = ", LETTERS[i]), add = TRUE)
+   if(i == 1 ){
+	axis(2, labels = seq(0,800, by = 100), at = seq(0,800, by = 100), 
+		las = 0 , col = "gray")
+	}		
+   if( i ==6 ){
+	legend("topleft", legend = c("Rejected","Admitted"),
+		col = c("lightblue1","lightblue3"),pch = 15,
+		border = "white",box.col = "white", pt.cex = 4,cex = 1.5)
+	}	
+}
+
+# 图14 图15
+
+# 图16 国际航班乘客示意图	
 
 AirPassengers2 <- matrix(AirPassengers, ncol = 12, byrow = TRUE,
             dimnames = list( 1948 + seq(12),
@@ -18,44 +162,46 @@ AirPassengers2 <- matrix(AirPassengers, ncol = 12, byrow = TRUE,
 barplot(colMeans(AirPassengers2),col = "lightblue",
 		border = "white",main = "Monthly Average Airline Passenger Numbers 1949-1960")
 		
-# 		
-set.seed(123456)
-barPois <- table(stats::rpois(1000, lambda = 5))
-barplot(barPois, col = "lightblue",border = "white",main = "The Poisson Distribution")
+# 图17 同图1
 
-# 复合条形图
-par(mfrow = c(1,6),mar = c(4.5,2.5,0,1.5))
-layout(mat = matrix(seq(6), ncol = 6, byrow = TRUE))
-for(i in seq(6)){
-plot(c(0,2),c(0,850), axes = FALSE, type = "n", xlab = "",ylab = "")
-barplot(UCBAdmissions[,,i], col = c("lightblue3","lightblue1"),axes = FALSE,
-		border = "white", xlab = paste0("Dept = ", LETTERS[i]), add = TRUE)
-if(i == 1 ){
-	axis(2, labels = seq(0,800, by = 100), at = seq(0,800, by = 100), 
-		las = 0 , col = "gray")
-	}		
-if( i ==6 ){
-legend("topleft", legend = c("Rejected","Admitted"),
-		col = c("lightblue1","lightblue3"),pch = 15,
-		border = "white",box.col = "white", pt.cex = 4,cex = 1.5)
-}	
-}
+# 图18
 
-# 点图
+# 图19
+
+# 图20
+
+# 图21 每月航班人数的变化						
+x <- 1948 + seq(12)
+y <- seq(from = 12 ,to = 1, length.out = 12)
+z <- matrix(AirPassengers, ncol = 12, byrow = TRUE,
+            dimnames = list( 1948 + seq(12),
+                    c("Jan", "Feb", "Mar", "Apr","May", "Jun",
+                    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec") ) )
+library(colormap)
+library(plot3D)
+colorBar <- colormap_pal(colormap=colormaps$viridis, reverse = TRUE,alpha = 1)(10)
+pdf(file="AirlinePassenger.pdf")
+par(mar =c(2,0,2,0) )
+hist3D(x, y, z, xlab = "Year", ylab = "Month", zlab = "# of Airline Passengers",
+	main = "Monthly Airline Passenger Numbers 1949-1960",
+	col = colorBar, colkey = FALSE,theta=30, phi=20,r=50,d=0.1,
+	expand = 0.5,ltheta=-55, lphi=180, lighting = TRUE,
+	ticktype = "detailed",space = .3, bty = "b2",facets = FALSE )
+dev.off()
+
+# 图22
+# 图23
+
+
+
+
+
+
+# 图10 的点图画法
 dotchart(VADeaths, main = "Death Rates in Virginia - 1940",bg="lightblue",
 			pch =21,color = "lightblue",lcolor = "gray",pt.cex=2)
 									
-# HairEyeColor 复合条形图 堆积条形图
-barplot(HairEyeColor[,,1]) # 对于 Male 四种颜色 每种四个梯度 green
-# 对于 Male也是 red 
-grep("blue",colors(),value = TRUE)
-grep("green",colors(),value = TRUE)
-grep("red",colors(),value = TRUE)
-grep("gray",colors(),value = TRUE)
-"darkslategray1" "darkslategray2" "darkslategray3" "darkslategray4" 
- "slategray1"     "slategray2"     "slategray3"     "slategray4"    
-
-###反例 复合堆积条形图  		
+# 复合堆积条形图的反例	
 pdf(file = "stackplot.pdf")
 layout(mat = matrix(seq(2), ncol = 2, byrow = TRUE))	
 plot(c(0,2),c(0,120), axes = FALSE, type = "n", xlab = "",ylab = "")		
@@ -69,15 +215,13 @@ legend(x= 1.2,y=120, legend = c("Black", "Brown", "Red", "Blond"),title = "Hair"
 		border = "white",box.col = "white", pt.cex = 3,cex = 1)	
 dev.off()
 
-# 马赛克图   矩阵树图
+# 用马赛克图代替复杂复合条形图  矩阵树图
 pdf(file = "stackmosaicplot.pdf")
 op <- par(mar = c(2,2,2.5,0))
 mosaicplot(HairEyeColor, col = "indianred", border = "white") 
 # 数据为多维数组"lightgray" "lightblue" indianred
 par(op)
 dev.off()
-
-mosaicplot(Titanic, col = "lightblue", border = "white" )
 
 # 复合条形图的另类
 pdf(file = "UCBAdmissions2.pdf",width=8)  # 马赛克图 强调 各系之间的对比
@@ -87,7 +231,7 @@ mosaicplot(~Dept + Admit + Gender,data = UCBAdmissions, col = "lightblue", borde
 par(op)
 dev.off()
 			
-## 点图
+# 图5 
 library(ggpubr)
 data("mtcars")
 dfm <- mtcars
@@ -97,7 +241,7 @@ dfm$cyl <- as.factor(dfm$cyl)
 dfm$name <- rownames(dfm)
 
 ggdotchart(dfm, x = "name", y = "mpg",
-		   xlab = "32 automobiles (1973–74 models)",
+           xlab = "32 automobiles (1973–74 models)",
            color = "cyl",                                # Color by groups
            palette = c("lightblue3", "lightblue2", "lightblue1"), # Custom color palette
            sorting = "descending",                       # Sort value in descending order
@@ -111,95 +255,11 @@ ggdotchart(dfm, x = "name", y = "mpg",
            ggtheme = theme_pubr()                        # ggplot2 theme
            )						
 						
-#### 3维柱形图						
-x <- 1948 + seq(12)
-y <- seq(from = 12 ,to = 1, length.out = 12)
-z <- matrix(AirPassengers, ncol = 12, byrow = TRUE,
-            dimnames = list( 1948 + seq(12),
-                    c("Jan", "Feb", "Mar", "Apr","May", "Jun",
-                    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec") ) )
-# library(colormap)
-library(plot3D)
-# colorBar <- colormap_pal(colormap=colormaps$viridis, 
-						# reverse = TRUE,alpha = 1)(10)
-par(mar =c(2,0,2,0) )
-hist3D(x, y, z, xlab = "Year", ylab = "Month", zlab = "",
-        main = "Monthly Airline Passenger Numbers 1949-1960",
-        col = "lightblue", colkey = FALSE, bty = "b2",theta = 30, phi = 20,
-        r = 50,d = 0.1, expand = 0.5, ltheta = -45, 
-		lphi = 180, lighting = FALSE,
-        ticktype = "detailed",space = .3,border = "whitesmoke" )
 
 
-## 点图  mtcars
-
-mtcars4 <- mtcars[mtcars$cyl == 4,]
-mtcars6 <- mtcars[mtcars$cyl == 6,]
-mtcars8 <- mtcars[mtcars$cyl == 8,]
-myData <- rbind( mtcars4[order(mtcars4$mpg,decreasing = FALSE ), ],
-				 mtcars6[order(mtcars6$mpg,decreasing = FALSE ), ],
-				 mtcars8[order(mtcars8$mpg,decreasing = FALSE ), ] )
-
-pdf(file = "dotplot.pdf",width=6,height=8)
-par(mar =c(2,8,0,4) )
-with(myData,{
-	barCenters <- barplot(mpg,horiz = TRUE,axes = FALSE,xlim=c(0,35),
-						col = rev(paste0("lightblue",seq(3)))[as.factor(cyl)],
-						# col = "white",
-						space =4.5, border = "white",width = .1)
-	text(y = barCenters, x = par("usr")[3],
-		 adj = 1, labels = rownames(myData), xpd = TRUE) # 纵轴标签
-	points(y = barCenters, x = mpg, pch= 19, cex = 2.5,
-			col = rev(paste0("lightblue",seq(3)))[as.factor(cyl)] )  # 加点
-	text(y = barCenters, x = mpg, labels = mpg,cex=.5) # 加数字
-	legend("topright", legend = c("8","6","4"),title = "cyl",
-			col = c("lightblue1","lightblue2","lightblue3"),pch = 19,
-			border = "white",box.col = "white", pt.cex = 2.5,cex = 1.5) # 加图例
-	axis(1, labels = seq(0,35,by=5), at = seq(0,35,by=5),  las = 1 , col = "gray")	# 横轴 	
-})
-dev.off()
-
-## 横躺条形图
-data(NCI60,package ="ISLR") # 加载数据		
-myData <- sort(table(NCI60$labs), decreasing = TRUE)		
-op <- par(mar=c(2,7,1,1))
-barCenters <- barplot(myData,col = "lightblue",axes = FALSE,
-			axisnames=FALSE,horiz=TRUE,border ="white")				
-text(y = barCenters, x = par("usr")[3],
-     adj = 1, labels = names(myData), xpd = TRUE)
-axis(1, labels = seq(0,9,by=1), at = seq(0,9,by=1),  las = 1 , col = "gray")
 
 
-### Gambia儿童疟疾
 
-library(sjPlot)
-library(sjmisc)
-library(ggplot2)
-# theme_set(theme_bw())
-
-data(gambia,package="geoR")
-
-# sjp.grpfrq(gambia$pos, gambia$treated, geom.colors = "Set1")
-
-dat1 <- subset(gambia,netuse==0) # 0 没有使用蚊帐
-dat1$Index <- rep(0,dim(dat1)[1])
-dat2 <- subset(gambia,netuse==1 & treated == 0)  # 1
-dat2$Index <- rep(1,dim(dat2)[1])
-dat3 <- subset(gambia,netuse==1 & treated == 1)  # 2 
-dat3$Index <- rep(2,dim(dat3)[1])
-mydata <- rbind(dat1,dat2,dat3)
-
-sjp.setTheme(theme.font="Times",
-			 geom.outline.color = "white",
-			 axis.linecolor = "white", 
-             base = theme_classic())
-
-pdf(file="bed_net.pdf",width = 6,height = 5.25)
-sjp.grpfrq(	mydata$Index,mydata$pos,  geom.colors = "Set1",
-			axis.titles="Childhood malaria in the Gambia",
-			axis.labels= c("net not use","net not treated","net treated"),
-			legend.title="RDT")
-dev.off()
 
 
 
