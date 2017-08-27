eb33a4ed750c5dc96e2eacc3e1c3f02183a3859f
Xiang-Yun Huang <11107100103@student.cumtb.edu.cn>
2017-08-27 12:56:16 +0800

    添加图11、12、14和15的代码

--- bar_code.R
+++ bar_code.R
@@ -167,7 +167,103 @@ barplot(t(VADeaths), col = rev(paste0("lightblue",seq(4))),
 				   box.col = "white", pt.cex = 2) )		
 axis(2, labels = seq(0,240,by=40), at = seq(0,240,by=40),  las = 1 , col = "gray")		
 		
-# 图11 图12	
+# 图11 这是gnuplot软件的绘图代码
+# set terminal pngcairo  transparent enhanced font "arial,10" fontscale 1.0 size 600, 400 
+# set output 'histograms.4.png'
+set bar 1.000000 front
+set boxwidth 0.75 absolute
+set style fill   solid 1.00 border lt -1
+set style circle radius graph 0.02, first 0.00000, 0.00000 
+set style ellipse size graph 0.05, 0.03, first 0.00000 angle 0 units xy
+set key outside right top vertical Left reverse noenhanced autotitle columnhead nobox
+set key invert samplen 4 spacing 1 width 0 height 0 
+set style histogram rowstacked title textcolor lt -1
+set style textbox transparent margins  1.0,  1.0 border
+unset logscale
+set datafile missing '-'
+set style data histograms
+set xtics border in scale 0,0 nomirror rotate by -45  autojustify
+set xtics  norangelimit 
+set xtics   ()
+unset paxis 1 tics
+unset paxis 2 tics
+unset paxis 3 tics
+unset paxis 4 tics
+unset paxis 5 tics
+unset paxis 6 tics
+unset paxis 7 tics
+set title "US immigration from Europe by decade\nPlot as stacked histogram" 
+set yrange [ 0.00000 : 7.00000e+06 ] noreverse nowriteback
+set paxis 1 range [ * : * ] noreverse nowriteback
+set paxis 2 range [ * : * ] noreverse nowriteback
+set paxis 3 range [ * : * ] noreverse nowriteback
+set paxis 4 range [ * : * ] noreverse nowriteback
+set paxis 5 range [ * : * ] noreverse nowriteback
+set paxis 6 range [ * : * ] noreverse nowriteback
+set paxis 7 range [ * : * ] noreverse nowriteback
+set colorbox vertical origin screen 0.9, 0.2, 0 size screen 0.05, 0.6, 0 front  noinvert bdefault
+x = 0.0
+i = 23
+## Last datafile plotted: "immigration.dat"
+plot 'immigration.dat' using 2:xtic(1), for [i=3:22] '' using i
+
+# 图12 这是gnuplot软件的绘图代码
+# set terminal pngcairo  transparent enhanced font "arial,10" fontscale 1.0 size 600, 400 
+# set output 'histograms.6.png'
+set bar 1.000000 front
+set border 3 front lt black linewidth 1.000 dashtype solid
+set boxwidth 0.75 absolute
+set style fill   solid 1.00 border lt -1
+set style circle radius graph 0.02, first 0.00000, 0.00000 
+set style ellipse size graph 0.05, 0.03, first 0.00000 angle 0 units xy
+set grid nopolar
+set grid noxtics nomxtics ytics nomytics noztics nomztics \
+ nox2tics nomx2tics noy2tics nomy2tics nocbtics nomcbtics
+set grid layerdefault   lt 0 linewidth 0.500,  lt 0 linewidth 0.500
+set key outside right top vertical Left reverse noenhanced autotitle columnhead box lt black linewidth 1.000 dashtype solid
+set style histogram columnstacked title textcolor lt -1
+set style textbox transparent margins  1.0,  1.0 border
+unset logscale
+set datafile missing '-'
+set style data histograms
+set xtics border in scale 1,0.5 nomirror norotate  autojustify
+set xtics  norangelimit 
+set xtics   ()
+set ytics border in scale 0,0 mirror norotate  autojustify
+set ztics border in scale 0,0 nomirror norotate  autojustify
+set cbtics border in scale 0,0 mirror norotate  autojustify
+set rtics axis in scale 0,0 nomirror norotate  autojustify
+set paxis 1 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 1 tics  rangelimit autofreq 
+set paxis 2 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 2 tics  rangelimit autofreq 
+set paxis 3 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 3 tics  rangelimit autofreq 
+set paxis 4 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 4 tics  rangelimit autofreq 
+set paxis 5 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 5 tics  rangelimit autofreq 
+set paxis 6 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 6 tics  rangelimit autofreq 
+set paxis 7 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 7 tics  rangelimit autofreq 
+set title "Immigration from Northern Europe\n(columstacked histogram)" 
+set xlabel "Country of Origin" 
+set ylabel "Immigration by decade" 
+set yrange [ 0.00000 : * ] noreverse nowriteback
+set paxis 1 range [ * : * ] noreverse nowriteback
+set paxis 2 range [ * : * ] noreverse nowriteback
+set paxis 3 range [ * : * ] noreverse nowriteback
+set paxis 4 range [ * : * ] noreverse nowriteback
+set paxis 5 range [ * : * ] noreverse nowriteback
+set paxis 6 range [ * : * ] noreverse nowriteback
+set paxis 7 range [ * : * ] noreverse nowriteback
+set colorbox vertical origin screen 0.9, 0.2, 0 size screen 0.05, 0.6, 0 front  noinvert bdefault
+x = 0.0
+i = 24
+## Last datafile plotted: "immigration.dat"
+plot 'immigration.dat' using 6 ti col, '' using 12 ti col,      '' using 13 ti col, '' using 14:key(1) ti col
+
 
 # 图13 复合条形图
 par(mfrow = c(1,6),mar = c(4.5,2.5,0,1.5))
@@ -187,7 +283,123 @@ for(i in seq(6)){
 	}	
 }
 
-# 图14 图15
+# 图14 这是 gnuplot软件的绘图代码 immigration.dat是软件自带的数据集
+# set terminal pngcairo  transparent enhanced font "arial,10" fontscale 1.0 size 600, 400 
+# set output 'histograms.7.png'
+set bar 1.000000 front
+set border 3 front lt black linewidth 1.000 dashtype solid
+set boxwidth 0.95 absolute
+set style fill   solid 1.00 noborder
+set style circle radius graph 0.02, first 0.00000, 0.00000 
+set style ellipse size graph 0.05, 0.03, first 0.00000 angle 0 units xy
+set grid nopolar
+set grid noxtics nomxtics ytics nomytics noztics nomztics \
+ nox2tics nomx2tics noy2tics nomy2tics nocbtics nomcbtics
+set grid layerdefault   lt 0 linewidth 0.500,  lt 0 linewidth 0.500
+set key bmargin center horizontal Left reverse noenhanced autotitle columnhead nobox
+set style histogram clustered gap 1 title textcolor lt -1 offset character 2, 0.25, 0
+set style textbox transparent margins  1.0,  1.0 border
+unset logscale
+set datafile missing '-'
+set style data histograms
+set xtics border in scale 0,0 nomirror rotate by -45  autojustify
+set xtics  norangelimit  font ",8"
+set xtics   ()
+set ytics border in scale 0,0 mirror norotate  autojustify
+set ytics  norangelimit autofreq  font ",8"
+set ztics border in scale 0,0 nomirror norotate  autojustify
+set cbtics border in scale 0,0 mirror norotate  autojustify
+set rtics axis in scale 0,0 nomirror norotate  autojustify
+set paxis 1 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 1 tics  rangelimit autofreq 
+set paxis 2 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 2 tics  rangelimit autofreq 
+set paxis 3 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 3 tics  rangelimit autofreq 
+set paxis 4 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 4 tics  rangelimit autofreq 
+set paxis 5 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 5 tics  rangelimit autofreq 
+set paxis 6 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 6 tics  rangelimit autofreq 
+set paxis 7 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 7 tics  rangelimit autofreq 
+set title "Immigration from different regions" 
+set xlabel " " 
+set xlabel  offset character 0, -2, 0 font "" textcolor lt -1 norotate
+set ylabel "Immigration by decade" 
+set paxis 1 range [ * : * ] noreverse nowriteback
+set paxis 2 range [ * : * ] noreverse nowriteback
+set paxis 3 range [ * : * ] noreverse nowriteback
+set paxis 4 range [ * : * ] noreverse nowriteback
+set paxis 5 range [ * : * ] noreverse nowriteback
+set paxis 6 range [ * : * ] noreverse nowriteback
+set paxis 7 range [ * : * ] noreverse nowriteback
+set colorbox vertical origin screen 0.9, 0.2, 0 size screen 0.05, 0.6, 0 front  noinvert bdefault
+x = 0.0
+i = 24
+## Last datafile plotted: "immigration.dat"
+plot newhistogram "Northern Europe", 'immigration.dat' using "Sweden":xtic(1) t col, '' u "Denmark" t col, '' u "Norway" t col, newhistogram "Southern Europe", '' u "Greece":xtic(1) t col, '' u "Romania" t col, '' u "Yugoslavia" t col, newhistogram "British Isles", '' u "Ireland":xtic(1) t col, '' u "United_Kingdom" t col
+
+# 图15 这是 gnuplot软件的绘图代码
+# set terminal pngcairo  transparent enhanced font "arial,10" fontscale 1.0 size 600, 400 
+# set output 'histograms.8.png'
+set bar 1.000000 front
+set border 3 front lt black linewidth 1.000 dashtype solid
+set boxwidth 0.8 absolute
+set style fill   solid 1.00 noborder
+set style circle radius graph 0.02, first 0.00000, 0.00000 
+set style ellipse size graph 0.05, 0.03, first 0.00000 angle 0 units xy
+set grid nopolar
+set grid noxtics nomxtics ytics nomytics noztics nomztics \
+ nox2tics nomx2tics noy2tics nomy2tics nocbtics nomcbtics
+set grid layerdefault   lt 0 linewidth 0.500,  lt 0 linewidth 0.500
+set key bmargin center horizontal Left reverse noenhanced autotitle columnhead nobox
+set style histogram rowstacked title textcolor lt -1 offset character 2, 0.25, 0
+set style textbox transparent margins  1.0,  1.0 border
+unset logscale
+set datafile missing '-'
+set style data histograms
+set xtics border in scale 0,0 nomirror rotate by -45  autojustify
+set xtics  norangelimit  font ",8"
+set xtics   ()
+set ytics border in scale 0,0 mirror norotate  autojustify
+set ytics  norangelimit autofreq  font ",8"
+set ztics border in scale 0,0 nomirror norotate  autojustify
+set cbtics border in scale 0,0 mirror norotate  autojustify
+set rtics axis in scale 0,0 nomirror norotate  autojustify
+set paxis 1 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 1 tics  rangelimit autofreq 
+set paxis 2 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 2 tics  rangelimit autofreq 
+set paxis 3 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 3 tics  rangelimit autofreq 
+set paxis 4 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 4 tics  rangelimit autofreq 
+set paxis 5 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 5 tics  rangelimit autofreq 
+set paxis 6 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 6 tics  rangelimit autofreq 
+set paxis 7 tics border in scale 0,0 nomirror norotate  autojustify
+set paxis 7 tics  rangelimit autofreq 
+set title "Immigration from different regions" 
+set xlabel " " 
+set xlabel  offset character 0, -2, 0 font "" textcolor lt -1 norotate
+set ylabel "Immigration by decade" 
+set yrange [ 0.00000 : 900000. ] noreverse nowriteback
+set paxis 1 range [ * : * ] noreverse nowriteback
+set paxis 2 range [ * : * ] noreverse nowriteback
+set paxis 3 range [ * : * ] noreverse nowriteback
+set paxis 4 range [ * : * ] noreverse nowriteback
+set paxis 5 range [ * : * ] noreverse nowriteback
+set paxis 6 range [ * : * ] noreverse nowriteback
+set paxis 7 range [ * : * ] noreverse nowriteback
+set colorbox vertical origin screen 0.9, 0.2, 0 size screen 0.05, 0.6, 0 front  noinvert bdefault
+x = 0.0
+i = 24
+## Last datafile plotted: "immigration.dat"
+plot newhistogram "Northern Europe", 'immigration.dat' using "Sweden":xtic(1) t col, '' u "Denmark" t col, '' u "Norway" t col, newhistogram "Southern Europe", '' u "Greece":xtic(1) t col, '' u "Romania" t col, '' u "Yugoslavia" t col, newhistogram "British Isles", '' u "Ireland":xtic(1) t col, '' u "United_Kingdom" t col
+
 
 # 图16 国际航班乘客示意图	
 
